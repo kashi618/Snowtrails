86537017cbc152d99dc23032f28249be52c2d21a
aleksy0000 <alexsszopinski4@gmail.com>
2024-12-03 11:05:22 +0000

    cleaned up code

--- Website/JS/script.js
+++ Website/JS/script.js
@@ -1,35 +1,35 @@
-function validateForm() {
-// Getting information about user
-let firstName  = document.getElementById("firstName").value;
-let lastName   = document.getElementById("lastName").value;
-let email      = document.getElementById("email").value;
-let cardNumber = document.getElementById("cardNumber").value;
-let cardExpiry = document.getElementById("cardExpiry").value;
-let cardCSV    = document.getElementById("cardCSV").value;
+    function validateForm() {
+        // Getting information about user
+        let firstName  = document.getElementById("firstName").value;
+        let lastName   = document.getElementById("lastName").value;
+        let email      = document.getElementById("email").value;
+        let cardNumber = document.getElementById("cardNumber").value;
+        let cardExpiry = document.getElementById("cardExpiry").value;
+        let cardCSV    = document.getElementById("cardCSV").value;
 
-if (firstName===""||lastName===""||email===""||cardNumber===""||cardExpiry===""|cardCSV==="") {
-	alert("One or more missing fields. Please try again later.");
-	return false;
-}
+        if (firstName===""||lastName===""||email===""||cardNumber===""||cardExpiry===""|cardCSV==="") {
+            alert("One or more missing fields. Please try again later.");
+            return false;
+    }
 
-// Validating email
-let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-if (!emailRegex.test(email)) {
-    alert("Please enter a valid email address");
-    return false;
-}
-return true;
-}
+    // Validating email
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Please enter a valid email address");
+            return false;
+        }
+        return true;
+    }
 
-function validateQuery() {
+    function validateQuery() {
 
-let firstName  = document.getElementById("firstName").value;
-let lastName   = document.getElementById("lastName").value;
-let email      = document.getElementById("email").value;
-let phone      = document.getElementById("phone").value;
+    let firstName  = document.getElementById("firstName").value;
+    let lastName   = document.getElementById("lastName").value;
+    let email      = document.getElementById("email").value;
+    let phone      = document.getElementById("phone").value;
 
 
-}
+    }
 
     //DROPDOWN MENU
     const toggleBtn = document.querySelector('.toggle_btn')
